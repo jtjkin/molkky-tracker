25e69271dfa5fa020d7f44499ec7d4a535ef819e
jtjkin <>
2020-06-09 01:14:52 +0300

    Updates

---
+++ precache-manifest.eebfd2335650b11d0658bf7c1725f8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f461d0739893a724838e854acf566d16",
+    "url": "/molkky-tracker/index.html"
+  },
+  {
+    "revision": "a59bc4ae12953cbf8005",
+    "url": "/molkky-tracker/static/css/main.7b0e887d.chunk.css"
+  },
+  {
+    "revision": "154ae4228cafb23ae2df",
+    "url": "/molkky-tracker/static/js/2.d0f2c720.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/molkky-tracker/static/js/2.d0f2c720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59bc4ae12953cbf8005",
+    "url": "/molkky-tracker/static/js/main.9c4c670d.chunk.js"
+  },
+  {
+    "revision": "85e58e688de98880f201",
+    "url": "/molkky-tracker/static/js/runtime-main.aa2d4143.js"
+  }
+]);
